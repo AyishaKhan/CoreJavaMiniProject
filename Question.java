c7e13d3a2b1a2461fa02c4145ea50ddbf1687d89
AyishaKhan <114661625+AyishaKhan@users.noreply.github.com>
2022-09-30 05:25:22 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,14 @@
+package miniproject;
+//create a class to represent questions for quiz application
+public class Question {
+	String prompt;//prompt for the question
+	String answer;
+	//question has two attributes that stores inside the class
+	public Question (String prompt,String answer)//we accept two paramaters
+	{
+		this.prompt=prompt;
+		this.answer=answer;
+	}
+
+	
+}
